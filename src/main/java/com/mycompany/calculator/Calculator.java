eccbed617a4ee5449233273684b1d55b6a27c51b
LeThiNhi050903 <lenhi050903@gmail.com>
2024-04-24 11:28:42 +0700

    commit 1

---
+++ src/main/java/com/mycompany/calculator/Calculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculator;
+
+/**
+ *
+ * @author 84943
+ */
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
